d19e2b3a146afce549c1e87832a60b29301c36c2
saket8 <saket@ecampusapp.com>
2024-03-09 21:17:29 +0530

    login removed

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -47,9 +47,6 @@ export default function Example() {
               {item.name}
             </a>
           ))}
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
         </div>
       </nav>
       <Dialog
@@ -90,14 +87,6 @@ export default function Example() {
                     {item.name}
                   </a>
                 ))}
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
               </div>
             </div>
           </div>
