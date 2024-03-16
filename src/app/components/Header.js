6e32cb55b1ee5710fc7793fa9a4515bb28c5a665
saket8 <saket@ecampusapp.com>
2024-03-16 18:35:15 +0530

    ui changes

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -75,6 +75,7 @@ export default function Header() {
                   <Link
                     key={item.name}
                     href={item.href}
+                    onClick={() => setMobileMenuOpen(false)}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-5"
                   >
                     {item.name}
